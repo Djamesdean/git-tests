a20d2e6c4a0be6b904bd2c0fb0179eb173974d03
Djames Eddine <djames.d@outlook.com>
2025-03-04 13:06:44 +0100

    resolve conflict in merging

--- test.py
+++ test.py
@@ -1,5 +1,8 @@
-print ("hello word")
+print ("hello word M")
+print ("Madou was here")
+print ("hello word D")
+print ("djames was here ")
 def calculate(a, b):
     return a + b
 
-calculate(1, 2)
+calculate(1, 2) 
